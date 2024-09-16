88462a8c1ebd72ec55c7bdc585f259d487bfb6fa
AmarinoLama <amarinolama@gmail.com>
2024-09-16 19:02:14 +0200

    feat (Ordenaciones) : añadir comentarios

--- src/main/java/org/example/Ordenaciones.java
+++ src/main/java/org/example/Ordenaciones.java
@@ -1,4 +1,16 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class Ordenaciones {
+    public static void main(String[] args) {
+
+    }
 }
+
+// lista.sort(Comparator.naturalOrder()); >>>>>>> ascendente case-sensitive
+// lista.sort(String.CASE_INSENSITIVE_ORDER); >>>>>>>> ascendente case-insensitive
+// lista.sort(Comparator.reverseOrder()); >>>>>> descendente case-sensitive
+// lista.sort(String.CASE_INSENSITIVE_ORDER.reversed()); >>>>>>>>> descendente case-insensitive
