5fafc121985e210a7a398210053eef1187c66d2b
Amán Lama Vilariño <a23amanlv@teis.intranet>
2024-09-16 13:43:52 +0200

    feat (): crear proyecto

---
+++ src/main/java/org/example/VSimpleLs.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.Scanner;
+
+public class VSimpleLs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nombre = sc.nextLine();
+        Path path = Path.of(nombre);
+
+        StringBuilder salida = new StringBuilder("");
+        salida.append(Files.isDirectory(path) ? "d" : "-")
+                .append(Files.isReadable(path) ? "r" : "-")
+                .append(Files.isWritable(path) ? "w" : "-")
+                .append((path.toFile().canRead() && path.toFile().canWrite() && path.toFile().canExecute()) ? "x" : "-")
+                .append(Files.isDirectory(path) ? " directorio" : " archivo");
+
+        System.out.printf(salida.toString());
+    }
+}
