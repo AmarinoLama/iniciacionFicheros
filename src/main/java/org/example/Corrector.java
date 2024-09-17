df3dc30d43f701f04045de40449e3ae20429f9fc
AmarinoLama <amarinolama@gmail.com>
2024-09-17 22:22:32 +0200

    feat () : añadir tabla porcentajes al ejercicio 4

--- src/main/java/org/example/Corrector.java
+++ src/main/java/org/example/Corrector.java
@@ -4,7 +4,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Corrector {
     public static void main(String[] args) {
@@ -12,15 +16,18 @@ public class Corrector {
         Scanner sc = new Scanner(System.in);
         Path path = Path.of(sc.nextLine());
 
-        String notas = "";
+        String calificaciones = "";
         String solTot = "";
+        ArrayList<String> notas = new ArrayList<>();
 
         try (BufferedReader lector = new BufferedReader(new
                 FileReader(path.toFile()));) {
             String linea;
-            while ((linea=lector.readLine()) != null) {
+            while ((linea = lector.readLine()) != null) {
                 if (solTot.length() != 0 && linea.length() != 0) {
-                    notas += linea + " Has sacado un : " + calcularNota(linea.substring(9,29), solTot) + "\n";
+                    calificaciones += "El alumno " + linea.substring(0, 7) + " has sacado un " + calcularNota(linea.substring(9, 29), solTot) +
+                            " lo que equibale a un " + darPuntuacion(calcularNota(linea.substring(9, 29), solTot)) + "\n";
+                    notas.add(darPuntuacion(calcularNota(linea.substring(9, 29), solTot)));
                 } else {
                     solTot += linea;
                 }
@@ -29,23 +36,54 @@ public class Corrector {
             System.err.println(e.getMessage());
         }
 
-        System.out.printf(solTot + "\n \n" + notas);
+        System.out.printf(solTot + "\n \n" + calificaciones +
+                "\n \n Tabla puntuaciones : \n" + tablaPorcentajes(notas).toString());
+
     }
 
     public static String calcularNota(String respuestas, String solTot) {
 
         Double nota = 0.0;
-        for (int i = 0; i<20; i++) {
-            String respuesta = respuestas.substring(i,i+1);
-            String solucion = solTot.substring(i,i+1);
+        for (int i = 0; i < 20; i++) {
+            String respuesta = respuestas.substring(i, i + 1);
+            String solucion = solTot.substring(i, i + 1);
             if (respuesta.equals(solucion)) {
                 nota += 0.5;
-            } else if (respuestas.substring(i, i+1).equals(" ")) {
+            } else if (respuestas.substring(i, i + 1).equals(" ")) {
                 nota += 0;
             } else {
                 nota -= 0.15;
             }
         }
-        return nota.toString();
+        return String.format("%.2f", nota); //esto sirve para redondear al segundo decimal
+    }
+
+    public static String darPuntuacion(String nota) {
+        double num = Double.parseDouble(nota.replace(',', '.'));
+        if (0.00 <= num && num <= 4.99) {
+            return "suspenso";
+        } else if (5.00 <= num && num <= 5.99) {
+            return "aprobado";
+        } else if (6.00 <= num && num <= 6.99) {
+            return "bien";
+        } else if (7.00 <= num && num <= 8.49) {
+            return "notable";
+        } else if (8.50 <= num && num <= 10.00) {
+            return "excelente";
+        }
+        return "something is wrong...";
+    }
+
+    public static HashMap<String, Double> tablaPorcentajes(ArrayList<String> calificaciones) {
+        HashMap<String, Double> tabla = new HashMap<>();
+
+        for (String calificacion : calificaciones) {
+            if (!tabla.containsKey(calificacion)) {
+                long repeticiones = calificaciones.stream().filter(x -> x.equals(calificacion)).count();
+                tabla.put(calificacion, ((double) repeticiones / calificaciones.size()) * 100);
+            }
+        }
+
+        return tabla;
     }
 }
